ee9c3b0ff04b1b0385abfe2176637303e92226cc
Simon Smart <simon.smart@ecmwf.int>
2016-03-10 09:23:06 +0000

    Add untracked files

---
+++ src/tree/TreeMultIO.cc
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Simon Smart
+/// @date   March 2016
+
+#include "tree/TreeMultIO.h"
+
+#include "eckit/exception/Exceptions.h"
+#include "eckit/io/MemoryHandle.h"
+
+
+using namespace eckit;
+using namespace multio;
+
+namespace treetool {
+
+// ------------------------------------------------------------------------------------------------
+
+namespace {
+
+    DataSinkBuilder<TreeMultIO> TreeMultIOBuilder("tree");
+
+}
+
+// ------------------------------------------------------------------------------------------------
+
+TreeMultIO::TreeMultIO(const Configuration& config) :
+    DataSink(config),
+    pool_(config.getString("path"), config.getLong("size")) {
+}
+
+TreeMultIO::~TreeMultIO() {
+}
+
+void TreeMultIO::write(DataBlobPtr blob, JournalRecordPtr record) {
+
+    if (record && journalAlways_) {
+        record->addWriteEntry(blob, id_);
+    }
+
+    try {
+
+        Log::info() << "Writing an object." << std::endl;
+
+    }
+    catch(Exception& e) {
+
+        // If something goes wrong, we need to journal (unless we have already journalled above)
+        if (record) {
+            if (!journalAlways_) record->addWriteEntry(blob, id_);
+        } else {
+            throw;
+        }
+    }
+}
+
+void TreeMultIO::print(std::ostream& os) const
+{
+    os << "TreeMultIO()";
+}
+
+// ------------------------------------------------------------------------------------------------
+
+} // treetool
---
+++ src/tree/TreeMultIO.h
@@ -0,0 +1,63 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Simon Smart
+/// @date March 2016
+
+
+#ifndef tree_TreeNode_H
+#define tree_TreeNode_H
+
+#include <iosfwd>
+
+#include "multio/DataSink.h"
+
+#include "eckit/filesystem/PathName.h"
+
+#include "tree/TreePool.h"
+
+namespace treetool {
+
+// ------------------------------------------------------------------------------------------------
+
+class TreeMultIO : public multio::DataSink {
+
+public: // methods
+
+    TreeMultIO(const eckit::Configuration& config);
+
+    virtual ~TreeMultIO();
+
+    virtual void write(eckit::DataBlobPtr blob, multio::JournalRecordPtr record);
+
+protected: // methods
+
+    virtual void print(std::ostream&) const;
+
+private: // friends
+
+    friend std::ostream &operator<<(std::ostream &s, const TreeMultIO &p) {
+        p.print(s);
+        return s;
+    }
+
+private: // members
+
+    eckit::PathName path_;
+    TreePool pool_;
+
+};
+
+// ------------------------------------------------------------------------------------------------
+
+} // namespace treetool
+
+#endif // tree_TreeNode_H
+
