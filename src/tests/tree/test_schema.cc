a66ed2ae0d876376565313e5d6f2f4d5b9d777b8
Simon Smart <simon.smart@ecmwf.int>
2016-03-29 19:06:04 +0100

    Add structure for doing some testing of pmem_tree

---
+++ src/tests/tree/test_schema.cc
@@ -0,0 +1,32 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#define BOOST_TEST_MODULE test_tree_schema
+
+#include "ecbuild/boost_test_framework.h"
+
+#include "eckit/exception/Exceptions.h"
+
+using namespace std;
+using namespace eckit;
+
+
+BOOST_AUTO_TEST_SUITE( test_tree_schema )
+
+//----------------------------------------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE( test_schema_todo )
+{
+    NOTIMP;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE_END()
