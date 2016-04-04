f606846437dc3b27ec37f8f27f49004a4970da03
Simon Smart <simon.smart@ecmwf.int>
2016-04-04 17:06:18 +0100

    Add first structure for persistent pool tests

---
+++ src/tests/pmem/test_persistent_pool.cc
@@ -0,0 +1,25 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#define BOOST_TEST_MODULE test_pmem
+
+#include <string>
+
+#include "ecbuild/boost_test_framework.h"
+
+using namespace std;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE( test_pmem_persistent_pool )
+
+//----------------------------------------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE_END()
